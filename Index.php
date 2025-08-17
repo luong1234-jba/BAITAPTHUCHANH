290c1d8fd0e76df91a6e55c8ee2bc26f7f0a54ea
luong123-jba <tranthiluong05072005@gmail.com>
2025-08-17 09:10:25 +0700

    baitap

---
+++ Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bai1</title>
+</head>
+<body>
+    <?php 
+    echo "<h1>Xin chào các bạn</h1>";
+    echo "Xin chào, tôi là trần thị lương"
+    ?>
+</body>
+</html>
